4c069236984b4cb77eec093933a4f629f8850fd7
Daniel Peters <daniel.peters.ch@gmail.com>
2018-06-02 20:33:13 +0200

    Remove build files. Update ignore

--- dist/lib/navigation/active-links.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    let pathname = window.location.pathname;
-    let element = document.querySelector('a[href="' + pathname + '"]').parentNode;
-    element.classList.add('active');
-});
